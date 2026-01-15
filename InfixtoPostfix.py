3ac8ef6655678dd8e88054ca1937d59ee8d9cbcf
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2026-01-15 06:28:02 +0600

    added infix to postfix

---
+++ InfixtoPostfix.py
@@ -0,0 +1,43 @@
+def precedence(op):
+    if op == '+' or op == '-':
+        return 1
+    if op == '*' or op == '/':
+        return 2
+    if op == '^':
+        return 3
+    return 0
+
+def infix_to_postfix(expression):
+    stack = []
+    postfix = ""
+
+    for ch in expression:
+        # If operand, add to postfix
+        if ch.isalnum():
+            postfix += ch
+
+        # If '(', push to stack
+        elif ch == '(':
+            stack.append(ch)
+
+        # If ')', pop until '('
+        elif ch == ')':
+            while stack and stack[-1] != '(':
+                postfix += stack.pop()
+            stack.pop()  # remove '('
+
+        # Operator
+        else:
+            while (stack and precedence(ch) <= precedence(stack[-1])):
+                postfix += stack.pop()
+            stack.append(ch)
+
+    # Pop remaining operators
+    while stack:
+        postfix += stack.pop()
+
+    return postfix
+
+# User input
+infix = input("Enter Infix Expression: ")
+print("Postfix Expression:", infix_to_postfix(infix))
